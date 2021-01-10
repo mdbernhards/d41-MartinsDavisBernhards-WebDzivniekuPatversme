b9fde4c9e3af6e2d3cd728f0f6bd4d6832f37846
Martins Bernhards <martinsdavisbernhards@gmail.com>
2021-01-10 16:14:43 +0200

    Added basic views for pages I have planned and controllers for them. Removed Privacy view.

---
+++ WebDzivniekuPatversme/WebDzivniekuPatversme/Controllers/ContactsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using WebDzivniekuPatversme.Models;
+
+namespace WebDzivniekuPatversme.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly ILogger<ContactsController> _logger;
+
+        public ContactsController(ILogger<ContactsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
