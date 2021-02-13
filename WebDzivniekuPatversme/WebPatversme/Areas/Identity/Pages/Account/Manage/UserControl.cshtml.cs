61a8f37b8f840ae92d1705a6f2f860108f6b3097
Martins Bernhards <martinsdavisbernhards@gmail.com>
2021-02-13 18:30:20 +0200

    Added User Control for admin role, can change other user roles, delete users. Little changes to filters .

--- WebDzivniekuPatversme/WebPatversme/Areas/Identity/Pages/Account/Manage/UserControl.cshtml.cs
+++ WebDzivniekuPatversme/WebPatversme/Areas/Identity/Pages/Account/Manage/UserControl.cshtml.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebDzivniekuPatversme.Areas.Identity.Pages.Account.Manage
@@ -20,29 +21,71 @@ namespace WebDzivniekuPatversme.Areas.Identity.Pages.Account.Manage
             _roleManager = roleManager;
         }
 
-        public class InputModel
-        {
-            private readonly UserManager<IdentityUser> _userManager;
-        }
-
         [BindProperty]
+        public List<UserWithRole> UsersWithRole { get; set; }
+
         public List<IdentityUser> Users { get; set; }
+
         public List<IdentityRole> Roles { get; set; }
 
+        public class UserWithRole
+        {
+            public IdentityUser User { get; set; }
+
+            public string Role { get; set; }
+        }
+
+        [Authorize(Roles = "administrator")]
         public async Task<IActionResult> OnGetAsync()
         {
             Users = _userManager.Users.ToList();
             Roles = _roleManager.Roles.ToList();
 
+            UsersWithRole = new List<UserWithRole>();
+
+            foreach (var user in Users)
+            {
+                UsersWithRole.Add(
+                    new UserWithRole() 
+                    { 
+                        Role = _userManager.GetRolesAsync(user).Result[0], 
+                        User = user
+                    });
+            }
+
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync()
+        [Authorize(Roles = "administrator")]
+        public async Task<IActionResult> OnPostChangeRolesAsync(string role, string userName)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+
+            var user = _userManager.FindByNameAsync(userName).Result;
+            var usersRoles = _userManager.GetRolesAsync(user).Result;
+
+            await _userManager.RemoveFromRolesAsync(
+                user,
+                usersRoles);
+
+            await _userManager.AddToRoleAsync(user, role);
+
+            return RedirectToPage();
+        }
+
+        [Authorize(Roles = "administrator")]
+        public async Task<IActionResult> OnPostDeleteUserAsync(string userName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = _userManager.FindByNameAsync(userName).Result;
+            await _userManager.DeleteAsync(user);
 
             return RedirectToPage();
         }
